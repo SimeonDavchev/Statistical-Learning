eb447a24cab3381a04dbfacc64490ff1250bd015
SimeonDavchev <bayernsimo@icloud.com>
2023-01-30 16:23:49 +0100

    v1.1 (added a plot to examine adjust and fixed -Inf value for dataset4)

--- ass2 /Ass2.R
+++ ass2 /Ass2.R
@@ -1,17 +1,30 @@
-##Preliminary notes in the dataset:
+##Preliminary notes in the dataset: ----
 
-#Data Set 1: The features are independent and normally distributed within each target class, with variances either being 1 or 10. Are the (population) covariance matrices the same across the target classes?
+#Data Set 1: The features are independent and normally distributed within each 
+#target class, with variances either being 1 or 10. Are the (population) 
+#covariance matrices the same across the target classes?
 #logistic
-#Data Set 2: The log odds ratio is a linear function of the features. Are the features normally distributed?
+
+#Data Set 2: The log odds ratio is a linear function of the features. 
+#Are the features normally distributed?
 #logistic
-#Data Set 3: The log odds ratio is a highly nonlinear - not quadratic- function of the features.
+
+#Data Set 3: The log odds ratio is a highly nonlinear - not quadratic- 
+#function of the features.
 #forest 
-#Data Set 4: The sample covariance matrices are close to the population covariance matrices in each target class. The features are normally distributed within each target class. Are the (population) covariance matrices the same across the target classes?
+
+#Data Set 4: The sample covariance matrices are close to the population 
+#covariance matrices in each target class. The features are normally distributed 
+#within each target class. Are the (population) covariance matrices the same 
+#across the target classes?
 #QDA
-#Data Set 5: The population covariance matrices among features are not equal but are very close across target classes. The features are normally distributed within each target class. 
+
+#Data Set 5: The population covariance matrices among features are not equal 
+#but are very close across target classes. The features are normally distributed 
+#within each target class. 
 #logistic
 
-#Initialise directory and answer sheet 
+#Initialise directory and answer sheet----
 rm(list = ls())
 setwd("~/Documents/Github/Statistical-Learning/ass2 /")
 answer_sheet=read.csv("Answer_Sheet.csv")
@@ -38,13 +51,24 @@ rf_fit= randomForest(target~.,data=dt3)
 rf_predict_logit= logit(predict(rf_fit,dt3))
 answer_sheet[,4]= rf_predict_logit
 
+
 #Dataset 4----
 library(MASS)
 dt4= read.csv("dataset4.csv")
 qda_fit= qda(target~., data=dt4)
 qda_predict= predict(qda_fit,dt4)
-qda_predict_logit= logit(qda_predict$posterior[,2],adjust=0)
+qda_predict_logit= logit(qda_predict$posterior[,2],adjust=1e-15)
 answer_sheet[,5]= qda_predict_logit
+
+#this is to figure out the adjust argument
+#min(qda_predict_logit)
+
+#library(ggplot2)
+#ggplot()+geom_line(aes(1:100,qda_predict_logit),color="red")+ 
+#  geom_line(aes(1:100,logit(qda_predict$posterior[,2],adjust=1e-10)),col="blue",linetype="dashed")+
+#  geom_line(aes(1:100,logit(qda_predict$posterior[,2],adjust=1e-5)),col="green",linetype="dashed")+
+#  geom_line(aes(1:100,logit(qda_predict$posterior[,2],adjust=0)),col="black",linetype="dotted")+
+#  geom_line(aes(1:100,logit(qda_predict$posterior[,2])),col="magenta")
 
 #Dataset 5----
 dt5= read.csv("dataset5.csv")
